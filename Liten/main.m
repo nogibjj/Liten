bc6b3bbe7dc9b2a797466ee78a7755523e7473a4
Noah Gift <noah.gift@gmail.com>
2011-09-05 22:28:26 -0700

    smoking old version

--- Liten/main.m
+++
@@ -1,9 +0,0 @@
-#import <Cocoa/Cocoa.h>
-#include <objc/objc-auto.h>
-
-int main(int argc, char *argv[])
-{
-	
-    objc_startCollectorThread();
-	return NSApplicationMain(argc, (const char **) argv);
-}
